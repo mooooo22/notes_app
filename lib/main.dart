8a09243b62c800987e4b3a24f3a540e2c123f87a
MoAdel <madel7586@gmail.com>
2024-03-30 12:30:57 +0200

    Add Flutter Bloc for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/notes_model.dart';
 import 'package:notes_app/views/home_view.dart';
-
 import 'views/edit_note_view.dart';
 
 void main() async {
@@ -19,18 +20,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          focusColor: Colors.blue,
-          brightness: Brightness.dark,
-          fontFamily: GoogleFonts.poppins().fontFamily,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNotesCubit(),
         ),
-        initialRoute: MyHomePage.id,
-        routes: {
-          EditNoteView.id: (context) => const EditNoteView(),
-          MyHomePage.id: (context) => const MyHomePage(),
-        });
+       
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            focusColor: Colors.blue,
+            brightness: Brightness.dark,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
+          initialRoute: MyHomePage.id,
+          routes: {
+            EditNoteView.id: (context) => const EditNoteView(),
+            MyHomePage.id: (context) => const MyHomePage(),
+          }),
+    );
   }
 }
